e5a57951f7d0cdc0e0ce44957e18db632936b647
valentinosander <valentinosander@gmail.com>
2025-06-18 10:58:04 -0300

    Main atualizado

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as S from './styledFooter';
+
+function Footer() {
+  return (
+    <S.FooterContainer>
+      <S.FooterBottom>
+        <S.FooterText>
+          © 2024 Borabico. Todos os direitos reservados.
+        </S.FooterText>
+      </S.FooterBottom>
+    </S.FooterContainer>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../../components/header/header';
+import CarrouselHome from '../../components/CarrouselHome/CarrouselHome';
+import Footer from '../../components/Footer/footer';
 
 function Home() {
 
   return(
     <>
     <Header />
+    <CarrouselHome />
+    <Footer />
     </>
   )
 }
